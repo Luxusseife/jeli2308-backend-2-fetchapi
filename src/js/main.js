94cb7d8dbdc0b05be54060376ec9a3331715bea4
Jenny Lind <jeli2308@student.miun.se>
2024-04-17 13:39:23 +0200

    Lagt till style på jobb-listan på index.html, lagt till funktionalitet för att hämta lagrade jobb från databasen.

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,48 @@ tablinks.forEach(tablink => {
         this.classList.add("active");
     });
 });
+
+
+// Funktionalitet för att hämta, radera och skapa jobb.
+
+// Funktion som kör vid sidladdning efter koll om element finns.
+window.onload = getJobs;
+
+// Funktion som hämtar lagrade jobb.
+async function getJobs() {
+
+    // API-URL.
+    const getUrl = "http://127.0.0.1:3000/work";
+
+    // AJAX-anrop, loopar genom data och skriver ut till skärm.
+    try {
+        const response = await fetch(getUrl);
+        const data = await response.json();
+
+        // Hämtar ul-elementet vari datan ska skrivas ut.
+        let jobListEl = document.getElementById("jobList");
+
+        // Loopar genom objekt-array och skriver ut jobb-poster.
+        data.forEach((job) => {
+            // Skapar ett listelement för varje jobb.
+            const jobItem = document.createElement("li");
+            jobItem.innerHTML = `
+                <h3>${job.companyname}</h3>
+                <p><strong>Ort:</strong> ${job.location}</p>
+                <p><strong>Befattning:</strong> ${job.jobtitle}</p>
+                <p><strong>Beskrivning:</strong> ${job.description}</p>
+                <form class="eraseButton">
+                    <input type="hidden" name="jobId" value="${job.workid}">
+                    <input type="submit" value="Radera" class="deleteJob">
+                </form>
+            `;
+    
+            // Lägger till listelementet i containern.
+            jobListEl.appendChild(jobItem);
+        });
+
+    // Felmeddelande.
+    } catch (error) {
+        console.log("Fetch failed. This message was created:", error);
+    }
+}
